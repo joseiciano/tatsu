0057f641ad14b39d96827777feef0f082d024c31
joseiciano <icianojn@gmail.com>
2026-06-29 11:09:34 -0500

    Update landing page hero and remove OpenSource section
    WHAT
    - Removed OpenSource component from the landing page
    - Updated hero headline from 'Orchestrate agents' to 'Swarm Control Agents'
    - Minor SVG formatting cleanup in hero buttons
    
    WHY
    - Simplify landing page by removing OpenSource section
    - Update branding to reflect 'Swarm Control Agents' positioning

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,7 +3,6 @@ import { Hero } from './components/Hero'
 import { DemoVideo } from './components/DemoVideo'
 import { FeatureCallouts } from './components/FeatureCallouts'
 import { Features } from './components/Features'
-import { OpenSource } from './components/OpenSource'
 import { Install } from './components/Install'
 import { Footer } from './components/Footer'
 import { ScrollDiorama } from './components/ScrollDiorama'
@@ -17,7 +16,6 @@ export function App() {
       <ScrollDiorama />
       <FeatureCallouts />
       <Features />
-      <OpenSource />
       <Install />
       <Footer />
     </div>
